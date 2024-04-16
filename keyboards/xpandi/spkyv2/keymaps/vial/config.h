2661e96bf23969baf7bcaa8b23ad86588b3f5fc2
hypan599 <qq450096325pe2@gmail.com>
2024-04-16 19:56:25 -0400

    Add spky keyboard

---
+++ keyboards/xpandi/spkyv2/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x69, 0xA0, 0xA4, 0x30, 0x70, 0x8E, 0x79, 0x35}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 6, 6 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 9 }
